e5171e634846a96ffa58537be9b23a5e1ec5a631
Nalipp <nateplipp@gmail.com>
2017-06-07 09:47:04 +0900

    remove and refresh github user

--- egghead_io/twitter_request/twitter_request.js
+++ egghead_io/twitter_request/twitter_request.js
@@ -3,12 +3,11 @@
 // var requestStream = Rx.Observable.of('https://api.github.com/users');
 
 var refreshButton = document.querySelector('.refresh');
-var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
-
 var closeButton1 = document.querySelector('.close1');
 var closeButton2 = document.querySelector('.close2');
 var closeButton3 = document.querySelector('.close3');
 
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
 var close1Clicks = Rx.Observable.fromEvent(closeButton1, 'click');
 var close2Clicks = Rx.Observable.fromEvent(closeButton2, 'click');
 var close3Clicks = Rx.Observable.fromEvent(closeButton3, 'click');
@@ -21,25 +20,30 @@ var requestOnRefreshStream = refreshClickStream
     return 'https://api.github.com/users?since=' + randomOffset;
 });
 
-// flatMap is now an alias for mergeMap 
-// but will work just the same.
-var responseStream = requestOnRefreshStream.merge(startupRequestStream)
+var requestStream = startupRequestStream.merge(requestOnRefreshStream);
+
+var responseStream = requestStream 
   .flatMap(requestUrl => {
     return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl))
   })
   .publishReplay().refCount(1);
 
-function createSuggestionStream(responseStream) {
-  return responseStream.map(listUser => 
-    listUser[Math.floor(Math.random()*listUser.length)]
-  )
-  .startWith(null)
-  .merge(refreshClickStream.map(ev => null));
-};
+function getRandomUser(listUsers) {
+  return listUsers[Math.floor(Math.random() * listUsers.length)];
+}
 
-var suggestion1Stream = createSuggestionStream(responseStream);
-var suggestion2Stream = createSuggestionStream(responseStream);
-var suggestion3Stream = createSuggestionStream(responseStream);
+function createSuggestionStream(responseStream, closeClickStream) {
+  return responseStream.map(getRandomUser)
+    .startWith(null)
+    .merge(refreshClickStream.map(ev => null))
+    .merge(
+      closeClickStream.withLatestFrom(responseStream, (x, R) => getRandomUser(R))
+    );
+}
+
+var suggestion1Stream = createSuggestionStream(responseStream, close1Clicks);
+var suggestion2Stream = createSuggestionStream(responseStream, close2Clicks);
+var suggestion3Stream = createSuggestionStream(responseStream, close3Clicks);
 
 function renderSuggestion(userData, selector) {
   var suggestionEl = document.querySelector(selector);
